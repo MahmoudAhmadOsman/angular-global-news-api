8c7ef036d922da9adf914ec6ca0440bcf74ac31f
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-05-04 10:10:39 -0500

    updated states component

--- src/app/components/states/states.component.ts
+++ src/app/components/states/states.component.ts
@@ -12,8 +12,8 @@ export class StatesComponent implements OnInit {
   public states: [];
   loading = true;
   constructor(private stateService: StateService) {
-    stateService.getAllStates().subscribe(async (data) => {
-      this.states = await data;
+    stateService.getAllStates().subscribe( (data) => {
+      this.states =  data;
       //Set loading to false after data is loaded
       this.loading = false;
       console.log("List of All Data: ", data);
